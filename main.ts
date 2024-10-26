444c5ff1e49e9b1a0a1c1e13a91773c976deadb2
adamanteye <yangzheh22@gmail.com>
2024-10-26 20:40:50 +0800

    fix: allow POST method

--- main.ts
+++ main.ts
@@ -3,10 +3,14 @@ export default {
     const u = new URL(req.url);
     const path = u.pathname;
     if (path === "/leaderboard") {
-      return fetch(env.LEADERBOARD);
-    }
-    else {
+      const requestOptions = {
+        method: req.method,
+        headers: req.headers,
+        body: req.method !== "GET" && req.method !== "HEAD" ? req.body : null,
+      };
+      return fetch(env.LEADERBOARD, requestOptions);
+    } else {
       return env.ASSETS.fetch(req);
     }
-  }
+  },
 };
